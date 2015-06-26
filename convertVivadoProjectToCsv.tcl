2f26d6c31ce1af5f46b058fb07cd75460d40338b
Hendrik Eeckhaut <hendrik.eeckhaut@sigasi.com>
2015-06-26 16:49:41 +0200

    Added tcl script to generate a csv file from a Vivado project

---
+++ convertVivadoProjectToCsv.tcl
@@ -0,0 +1,14 @@
+#Vivado script that creates a csv file ("vivado_files.csv") with a list of all VHDL
+#and Verilog files in a Vivado project + their library name
+#
+#How to use
+#vivado -mode batch -source convertVivadoProjectToCsv.tcl <VivadoProjectFile.xpr>
+
+#open_project -read_only -quiet project_1.xpr
+set source_files [get_files -filter {(FILE_TYPE == VHDL || FILET_TYPE == VERILOG ) && USED_IN_SIMULATION == 1 } ]
+set csv_file [open "vivado_files.csv" w]
+foreach source_file $source_files {
+	puts  $csv_file [ concat  [ get_property LIBRARY $source_file ] "," $source_file ]
+}
+#close_project
+

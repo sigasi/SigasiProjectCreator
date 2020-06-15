ac7e959a3faf3d727ec92181b732c79f6ccffc34
Bart Brosens <bart.brosens@sigasi.com>
2020-06-15 17:16:30 +0200

    Including VHDL 2008 files to the export from Vivado

--- src/SigasiProjectCreator/convertVivadoProjectToCsv.tcl
+++ src/SigasiProjectCreator/convertVivadoProjectToCsv.tcl
@@ -5,7 +5,7 @@
 #vivado -mode batch -source convertVivadoProjectToCsv.tcl <VivadoProjectFile.xpr>
 
 #open_project -read_only -quiet project_1.xpr
-set source_files [get_files -filter {(FILE_TYPE == VHDL || FILE_TYPE == VERILOG || FILE_TYPE == SYSTEMVERILOG) && USED_IN_SIMULATION == 1 } ]
+set source_files [get_files -filter {(FILE_TYPE == VHDL || FILE_TYPE == "VHDL 2008" || FILE_TYPE == VERILOG || FILE_TYPE == SYSTEMVERILOG) && USED_IN_SIMULATION == 1 } ]
 set csv_file [open "vivado_files.csv" w]
 foreach source_file $source_files {
 	puts  $csv_file [ concat  [ get_property LIBRARY $source_file ] "," $source_file ]

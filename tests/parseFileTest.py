b78b8320aa448c1d85a6de2599140b6b54bf7c04
Wim Meeus <Wim.Meeus@sigasi.com>
2021-01-27 17:45:02 +0100

    Reorganized DotF parser unit tests
    re #21

--- tests/parseFileTest.py
+++ tests/parseFileTest.py
@@ -4,7 +4,7 @@ from parseFile import parse_dotf
 class test_parseFile(unittest.TestCase):
     
     def test_basic(self):
-        result = parse_dotf("testfiles/test1.f")
+        result = parse_dotf("tests/test-files/dotFparser/test1.f")
         expected = [
         ['+incdir', '+../../../../rtl/verilog/pkg/'],
         ['+incdir', '+../../../../bench/verilog/baremetal/'],
@@ -50,7 +50,7 @@ class test_parseFile(unittest.TestCase):
         self.assertListEqual(result, expected)
 
     def test_continuation(self):
-        result = parse_dotf("testfiles/continuation.f")
+        result = parse_dotf("tests/test-files/dotFparser/continuation.f")
         expected = [
             ['-makelib ies_lib/xilinx_vip -sv', "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_axi4pc.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/xil_common_vip_pkg.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_pkg.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_if.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_if.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/clk_vip_if.sv", "D:/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/rst_vip_if.sv", '-endlib'],
             ['-makelib ies_lib/xil_defaultlib -sv',  "D:/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv", "D:/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv", "D:/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv", '-endlib'],
@@ -60,7 +60,7 @@ class test_parseFile(unittest.TestCase):
         self.assertListEqual(result, expected)
 
     def test_filelist(self):
-        result = parse_dotf("testfiles/filelist.f")
+        result = parse_dotf("tests/test-files/dotFparser/filelist.f")
         expected = [
             '../../../ipstatic/axi_protocol_converter_v2_1/hdl/verilog/axi_protocol_converter_v2_1_axi3_conv.v',
             '../../../ipstatic/axi_protocol_converter_v2_1/hdl/verilog/axi_protocol_converter_v2_1_axilite_conv.v',
@@ -88,7 +88,7 @@ class test_parseFile(unittest.TestCase):
         self.assertListEqual(result, expected)
 
     def test_variable(self):
-        result = parse_dotf("testfiles/variable.f")
+        result = parse_dotf("tests/test-files/dotFparser/variable.f")
         expected = [
             ['+incdir', '+$VW_GO2UVM_HOME/src'],
             '${VW_GO2UVM_HOME}/src/vw_go2uvm_pkg.sv',
@@ -100,7 +100,7 @@ class test_parseFile(unittest.TestCase):
         self.assertListEqual(result, expected)
 
     def test_wildcard(self):
-        result = parse_dotf("testfiles/wildcard.f")
+        result = parse_dotf("tests/test-files/dotFparser/wildcard.f")
         expected = [
             '-smartorder -work work -V93 -top tb_image -gui -access +rw -maxdelays -sdf_cmd_file ./sdf_cmd.cmd',
             '/usr/eda/dk/ibm/cmrf7sf/digital_20111130/verilog/*.v',

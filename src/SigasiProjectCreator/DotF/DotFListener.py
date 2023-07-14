dae892eb99a42a666e63d9689c3955dcee3b4729
Wim Meeus <Wim.Meeus@sigasi.com>
2023-07-14 11:48:08 +0200

    Improve .f import
    re #21

--- src/SigasiProjectCreator/DotF/DotFListener.py
+++ src/SigasiProjectCreator/DotF/DotFListener.py
@@ -1,6 +1,6 @@
-# Generated from DotF.g4 by ANTLR 4.11.1
+# Generated from DotF.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .DotFParser import DotFParser
 else:
     from DotFParser import DotFParser
--- src/SigasiProjectCreator/DotF/DotFParser.py
+++ src/SigasiProjectCreator/DotF/DotFParser.py
@@ -1,4 +1,4 @@
-# Generated from DotF.g4 by ANTLR 4.11.1
+# Generated from DotF.g4 by ANTLR 4.13.0
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -10,29 +10,22 @@ else:
 
 def serializedATN():
     return [
-        4,1,14,76,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,2,5,7,5,2,6,7,
-        6,2,7,7,7,1,0,5,0,18,8,0,10,0,12,0,21,9,0,1,1,1,1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,5,1,32,8,1,10,1,12,1,35,9,1,1,1,1,1,1,1,1,1,5,1,41,
-        8,1,10,1,12,1,44,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-        3,1,57,8,1,1,2,1,2,1,3,1,3,1,4,1,4,1,5,1,5,1,6,1,6,3,6,69,8,6,1,
-        6,1,6,1,7,1,7,1,7,1,7,0,0,8,0,2,4,6,8,10,12,14,0,1,1,0,6,7,77,0,
-        19,1,0,0,0,2,56,1,0,0,0,4,58,1,0,0,0,6,60,1,0,0,0,8,62,1,0,0,0,10,
-        64,1,0,0,0,12,66,1,0,0,0,14,72,1,0,0,0,16,18,3,2,1,0,17,16,1,0,0,
-        0,18,21,1,0,0,0,19,17,1,0,0,0,19,20,1,0,0,0,20,1,1,0,0,0,21,19,1,
-        0,0,0,22,23,3,10,5,0,23,24,5,11,0,0,24,57,1,0,0,0,25,26,3,10,5,0,
-        26,27,3,14,7,0,27,28,3,2,1,0,28,57,1,0,0,0,29,33,3,4,2,0,30,32,3,
-        6,3,0,31,30,1,0,0,0,32,35,1,0,0,0,33,31,1,0,0,0,33,34,1,0,0,0,34,
-        36,1,0,0,0,35,33,1,0,0,0,36,37,5,11,0,0,37,57,1,0,0,0,38,42,3,4,
-        2,0,39,41,3,6,3,0,40,39,1,0,0,0,41,44,1,0,0,0,42,40,1,0,0,0,42,43,
-        1,0,0,0,43,45,1,0,0,0,44,42,1,0,0,0,45,46,3,12,6,0,46,47,3,2,1,0,
-        47,57,1,0,0,0,48,49,3,8,4,0,49,50,5,11,0,0,50,57,1,0,0,0,51,52,3,
-        8,4,0,52,53,3,12,6,0,53,54,3,2,1,0,54,57,1,0,0,0,55,57,5,11,0,0,
-        56,22,1,0,0,0,56,25,1,0,0,0,56,29,1,0,0,0,56,38,1,0,0,0,56,48,1,
-        0,0,0,56,51,1,0,0,0,56,55,1,0,0,0,57,3,1,0,0,0,58,59,5,2,0,0,59,
-        5,1,0,0,0,60,61,5,2,0,0,61,7,1,0,0,0,62,63,5,3,0,0,63,9,1,0,0,0,
-        64,65,7,0,0,0,65,11,1,0,0,0,66,68,5,1,0,0,67,69,5,10,0,0,68,67,1,
-        0,0,0,68,69,1,0,0,0,69,70,1,0,0,0,70,71,5,11,0,0,71,13,1,0,0,0,72,
-        73,5,8,0,0,73,74,5,11,0,0,74,15,1,0,0,0,5,19,33,42,56,68
+        4,1,14,52,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,2,5,7,5,2,6,7,
+        6,2,7,7,7,1,0,5,0,18,8,0,10,0,12,0,21,9,0,1,1,1,1,3,1,25,8,1,1,1,
+        1,1,3,1,29,8,1,1,1,1,1,3,1,33,8,1,1,2,1,2,1,3,1,3,1,4,1,4,1,5,1,
+        5,1,6,1,6,3,6,45,8,6,1,6,1,6,1,7,1,7,1,7,1,7,0,0,8,0,2,4,6,8,10,
+        12,14,0,1,1,0,6,7,50,0,19,1,0,0,0,2,32,1,0,0,0,4,34,1,0,0,0,6,36,
+        1,0,0,0,8,38,1,0,0,0,10,40,1,0,0,0,12,42,1,0,0,0,14,48,1,0,0,0,16,
+        18,3,2,1,0,17,16,1,0,0,0,18,21,1,0,0,0,19,17,1,0,0,0,19,20,1,0,0,
+        0,20,1,1,0,0,0,21,19,1,0,0,0,22,24,3,4,2,0,23,25,3,6,3,0,24,23,1,
+        0,0,0,24,25,1,0,0,0,25,33,1,0,0,0,26,28,3,8,4,0,27,29,3,10,5,0,28,
+        27,1,0,0,0,28,29,1,0,0,0,29,33,1,0,0,0,30,33,3,10,5,0,31,33,5,11,
+        0,0,32,22,1,0,0,0,32,26,1,0,0,0,32,30,1,0,0,0,32,31,1,0,0,0,33,3,
+        1,0,0,0,34,35,5,2,0,0,35,5,1,0,0,0,36,37,5,2,0,0,37,7,1,0,0,0,38,
+        39,5,3,0,0,39,9,1,0,0,0,40,41,7,0,0,0,41,11,1,0,0,0,42,44,5,1,0,
+        0,43,45,5,10,0,0,44,43,1,0,0,0,44,45,1,0,0,0,45,46,1,0,0,0,46,47,
+        5,11,0,0,47,13,1,0,0,0,48,49,5,8,0,0,49,50,5,11,0,0,50,15,1,0,0,
+        0,5,19,24,28,32,44
     ]
 
 class DotFParser ( Parser ):
@@ -81,7 +74,7 @@ class DotFParser ( Parser ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.11.1")
+        self.checkVersion("4.13.0")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
@@ -126,7 +119,7 @@ class DotFParser ( Parser ):
             self.state = 19
             self._errHandler.sync(self)
             _la = self._input.LA(1)
-            while ((_la) & ~0x3f) == 0 and ((1 << _la) & 2252) != 0:
+            while (((_la) & ~0x3f) == 0 and ((1 << _la) & 2252) != 0):
                 self.state = 16
                 self.cmd_option()
                 self.state = 21
@@ -149,39 +142,24 @@ class DotFParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
+        def plus_option(self):
+            return self.getTypedRuleContext(DotFParser.Plus_optionContext,0)
+
+
+        def plus_option_arg(self):
+            return self.getTypedRuleContext(DotFParser.Plus_option_argContext,0)
+
+
+        def dash_option(self):
+            return self.getTypedRuleContext(DotFParser.Dash_optionContext,0)
+
+
         def filename(self):
             return self.getTypedRuleContext(DotFParser.FilenameContext,0)
 
 
         def NL(self):
             return self.getToken(DotFParser.NL, 0)
-
-        def filecont(self):
-            return self.getTypedRuleContext(DotFParser.FilecontContext,0)
-
-
-        def cmd_option(self):
-            return self.getTypedRuleContext(DotFParser.Cmd_optionContext,0)
-
-
-        def plus_option(self):
-            return self.getTypedRuleContext(DotFParser.Plus_optionContext,0)
-
-
-        def plus_option_arg(self, i:int=None):
-            if i is None:
-                return self.getTypedRuleContexts(DotFParser.Plus_option_argContext)
-            else:
-                return self.getTypedRuleContext(DotFParser.Plus_option_argContext,i)
-
-
-        def continuation(self):
-            return self.getTypedRuleContext(DotFParser.ContinuationContext,0)
-
-
-        def dash_option(self):
-            return self.getTypedRuleContext(DotFParser.Dash_optionContext,0)
-
 
         def getRuleIndex(self):
             return DotFParser.RULE_cmd_option
@@ -201,91 +179,48 @@ class DotFParser ( Parser ):
 
         localctx = DotFParser.Cmd_optionContext(self, self._ctx, self.state)
         self.enterRule(localctx, 2, self.RULE_cmd_option)
-        self._la = 0 # Token type
         try:
-            self.state = 56
+            self.state = 32
             self._errHandler.sync(self)
-            la_ = self._interp.adaptivePredict(self._input,3,self._ctx)
-            if la_ == 1:
+            token = self._input.LA(1)
+            if token in [2]:
                 self.enterOuterAlt(localctx, 1)
                 self.state = 22
-                self.filename()
-                self.state = 23
-                self.match(DotFParser.NL)
-                pass
+                self.plus_option()
+                self.state = 24
+                self._errHandler.sync(self)
+                la_ = self._interp.adaptivePredict(self._input,1,self._ctx)
+                if la_ == 1:
+                    self.state = 23
+                    self.plus_option_arg()
 
-            elif la_ == 2:
+
+                pass
+            elif token in [3]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 25
-                self.filename()
                 self.state = 26
-                self.filecont()
-                self.state = 27
-                self.cmd_option()
-                pass
+                self.dash_option()
+                self.state = 28
+                self._errHandler.sync(self)
+                la_ = self._interp.adaptivePredict(self._input,2,self._ctx)
+                if la_ == 1:
+                    self.state = 27
+                    self.filename()
 
-            elif la_ == 3:
+
+                pass
+            elif token in [6, 7]:
                 self.enterOuterAlt(localctx, 3)
-                self.state = 29
-                self.plus_option()
-                self.state = 33
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-                while _la==2:
-                    self.state = 30
-                    self.plus_option_arg()
-                    self.state = 35
-                    self._errHandler.sync(self)
-                    _la = self._input.LA(1)
-
-                self.state = 36
-                self.match(DotFParser.NL)
+                self.state = 30
+                self.filename()
                 pass
-
-            elif la_ == 4:
+            elif token in [11]:
                 self.enterOuterAlt(localctx, 4)
-                self.state = 38
-                self.plus_option()
-                self.state = 42
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-                while _la==2:
-                    self.state = 39
-                    self.plus_option_arg()
-                    self.state = 44
-                    self._errHandler.sync(self)
-                    _la = self._input.LA(1)
-
-                self.state = 45
-                self.continuation()
-                self.state = 46
-                self.cmd_option()
-                pass
-
-            elif la_ == 5:
-                self.enterOuterAlt(localctx, 5)
-                self.state = 48
-                self.dash_option()
-                self.state = 49
+                self.state = 31
                 self.match(DotFParser.NL)
                 pass
-
-            elif la_ == 6:
-                self.enterOuterAlt(localctx, 6)
-                self.state = 51
-                self.dash_option()
-                self.state = 52
-                self.continuation()
-                self.state = 53
-                self.cmd_option()
-                pass
-
-            elif la_ == 7:
-                self.enterOuterAlt(localctx, 7)
-                self.state = 55
-                self.match(DotFParser.NL)
-                pass
-
+            else:
+                raise NoViableAltException(self)
 
         except RecognitionException as re:
             localctx.exception = re
@@ -326,7 +261,7 @@ class DotFParser ( Parser ):
         self.enterRule(localctx, 4, self.RULE_plus_option)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 58
+            self.state = 34
             self.match(DotFParser.PLUS_OPTION)
         except RecognitionException as re:
             localctx.exception = re
@@ -367,7 +302,7 @@ class DotFParser ( Parser ):
         self.enterRule(localctx, 6, self.RULE_plus_option_arg)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 60
+            self.state = 36
             self.match(DotFParser.PLUS_OPTION)
         except RecognitionException as re:
             localctx.exception = re
@@ -408,7 +343,7 @@ class DotFParser ( Parser ):
         self.enterRule(localctx, 8, self.RULE_dash_option)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 62
+            self.state = 38
             self.match(DotFParser.DASH_OPTION)
         except RecognitionException as re:
             localctx.exception = re
@@ -453,7 +388,7 @@ class DotFParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 64
+            self.state = 40
             _la = self._input.LA(1)
             if not(_la==6 or _la==7):
                 self._errHandler.recoverInline(self)
@@ -503,17 +438,17 @@ class DotFParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 66
+            self.state = 42
             self.match(DotFParser.T__0)
-            self.state = 68
+            self.state = 44
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             if _la==10:
-                self.state = 67
+                self.state = 43
                 self.match(DotFParser.WS)
 
 
-            self.state = 70
+            self.state = 46
             self.match(DotFParser.NL)
         except RecognitionException as re:
             localctx.exception = re
@@ -557,9 +492,9 @@ class DotFParser ( Parser ):
         self.enterRule(localctx, 14, self.RULE_filecont)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 72
+            self.state = 48
             self.match(DotFParser.CONT)
-            self.state = 73
+            self.state = 49
             self.match(DotFParser.NL)
         except RecognitionException as re:
             localctx.exception = re

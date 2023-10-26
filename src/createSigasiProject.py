38f8f0eee9bca806949452daa0b276ea4940229a
Wim Meeus <Wim.Meeus@sigasi.com>
2023-10-26 14:47:33 +0200

    Fix top level script
    re #23

--- src/createSigasiProject.py
+++ src/createSigasiProject.py
@@ -2,13 +2,14 @@
     :copyright: (c) 2008-2023 Sigasi
     :license: BSD, see LICENSE for more details.
 """
-from SigasiProjectCreator import ProjectCreator
+from SigasiProjectCreator.ProjectCreator import ProjectCreator
 from SigasiProjectCreator.ProjectOptions import ProjectOptions
 
 
 def main():
     project_options = ProjectOptions()
-    ProjectCreator(project_options).create_project()
+    project_creator = ProjectCreator(project_options)
+    project_creator.create_project()
 
 
 if __name__ == '__main__':
